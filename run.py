77778440bd8ff8205f73dfdb20f73be8ee58e143
Luis Martinez de Bartolome <lasote@gmail.com>
2019-09-23 16:35:50 +0200

    with sessiong

--- run.py
+++ run.py
@@ -3,6 +3,16 @@ import logging
 import os
 
 
+def with_session():
+    class UTF8RedirectingSession(requests.Session):
+        def get_redirect_target(self, resp):
+            if resp.is_redirect:
+                return resp.headers['location'].encode('latin1').decode('utf8')
+            return None
+
+    with UTF8RedirectingSession() as session:
+        session.get("https://api.bintray.com:443/conan/conan/conan-center/v1/files/conan/OpenSSL/1.1.1c/stable/0/package/8f5c5dedfae9faebaa2d65d5c8f43d2ec7d219de/0/conan_package.tgz")
+
 if __name__ == "__main__":
     logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)
@@ -10,8 +20,8 @@ if __name__ == "__main__":
     requests_log.setLevel(logging.DEBUG)
     requests_log.propagate = True
     
-    response = requests.get("https://api.bintray.com:443/conan/conan/conan-center/v1/files/conan/OpenSSL/1.1.1c/stable/0/package/8f5c5dedfae9faebaa2d65d5c8f43d2ec7d219de/0/conan_package.tgz")
-
+    #response = requests.get("https://api.bintray.com:443/conan/conan/conan-center/v1/files/conan/OpenSSL/1.1.1c/stable/0/package/8f5c5dedfae9faebaa2d65d5c8f43d2ec7d219de/0/conan_package.tgz")
+    with_session()
 
 
 #   if not response.ok:
