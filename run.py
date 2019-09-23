e67f2bb7004702e717c55920c121c5455c00f33a
Luis Martinez de Bartolome <lasote@gmail.com>
2019-09-23 11:49:09 +0200

    w?

--- run.py
+++ run.py
@@ -12,8 +12,6 @@ if __name__ == "__main__":
     
     response = requests.get("https://api.bintray.com:443/conan/conan/conan-center/v1/files/conan/OpenSSL/1.1.1c/stable/0/package/8f5c5dedfae9faebaa2d65d5c8f43d2ec7d219de/0/conan_package.tgz")
     if not response.ok:
-        print(auth.token)
-        print(headers)
         print(response)
         print(response.status_code)
         print(response.content)
