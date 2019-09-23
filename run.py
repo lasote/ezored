c32758d4c105b7c1254b5daba50dbb5e7bc63396
Luis Martinez de Bartolome <lasote@gmail.com>
2019-09-23 17:14:39 +0200

    asasd

--- run.py
+++ run.py
@@ -8,14 +8,16 @@ def with_session():
         def get_redirect_target(self, resp):
             if resp.is_redirect:
                 # print(resp.headers['location'])
-                print("\n\n return modified \n\n")
-                print(resp.headers['location'])
-                print(resp.headers['location'].__class__)
-                return resp.headers['location'].replace("%7E", "~").encode("latin1").decode("utf-8")
+                #print("\n\n return modified \n\n")
+                #print(resp.headers['location'])
+                #print(resp.headers['location'].__class__)
+                return resp.headers['location'] #.replace("%7E", "~").encode("latin1").decode("utf-8")
             return None
 
     with UTF8RedirectingSession() as session:
-        session.get("https://api.bintray.com:443/conan/conan/conan-center/v1/files/conan/OpenSSL/1.1.1c/stable/0/package/8f5c5dedfae9faebaa2d65d5c8f43d2ec7d219de/0/conan_package.tgz")
+        headers = {}
+        headers["Pragma"] = "Pragma: akamai-x-get-client-ip, akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no, akamai-x-get-request-id"
+        return session.get("https://api.bintray.com:443/conan/conan/conan-center/v1/files/conan/OpenSSL/1.1.1c/stable/0/package/8f5c5dedfae9faebaa2d65d5c8f43d2ec7d219de/0/conan_package.tgz", headers=headers)
 
 if __name__ == "__main__":
     logging.basicConfig()
@@ -24,9 +26,12 @@ if __name__ == "__main__":
     requests_log.setLevel(logging.DEBUG)
     requests_log.propagate = True
     
+    
     #response = requests.get("https://api.bintray.com:443/conan/conan/conan-center/v1/files/conan/OpenSSL/1.1.1c/stable/0/package/8f5c5dedfae9faebaa2d65d5c8f43d2ec7d219de/0/conan_package.tgz")
-    with_session()
 
+    ret = with_session()
+    while not ret.ok:
+        with_session()
 
 #   if not response.ok:
 #        print(response)
