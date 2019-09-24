6b632ac0a21e2faa3dc879a6221e06e7ec52ed1b
Luis Martinez de Bartolome <lasote@gmail.com>
2019-09-24 08:45:46 +0200

    Try this trick

--- run2.py
+++ run2.py
@@ -1,7 +1,9 @@
-import requests
+import http.client
 import logging
-import os
-import http.client 
+import urllib
+
+import requests
+import urllib3
 
 http.client.HTTPConnection.debuglevel = 5
 
@@ -11,5 +13,14 @@ requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(1)
 requests_log.propagate = True
 
-requests.get("https://api.bintray.com:443/conan/conan/conan-center/v1/files/conan/OpenSSL/1.1.1c/stable/0/package/8f5c5dedfae9faebaa2d65d5c8f43d2ec7d219de/0/conan_package.tgz")
+
+class NoQuotedCommasSession(requests.Session):
+    def send(self, *a, **kw):
+        # a[0] is prepared request
+        a[0].url = a[0].url.replace("%7E", "~")
+        return requests.Session.send(self, *a, **kw)
+
+
+s = NoQuotedCommasSession()
+s.get("https://api.bintray.com:443/conan/conan/conan-center/v1/files/conan/OpenSSL/1.1.1c/stable/0/package/8f5c5dedfae9faebaa2d65d5c8f43d2ec7d219de/0/conan_package.tgz")
 
